4309317dcf9a0bed6e9a0514e7aab715028556d5
Flame753 <Sgurman753@gmail.com>
2021-12-15 22:56:27 -0500

    Refactered items to be more flexible

--- items.py
+++ items.py
@@ -1,66 +1,106 @@
-class Weapon:
-    def __init__(self) -> None:
-        raise NotImplementedError("Do not create raw Weapon objects.")
+from abc import ABC
 
-    def __str__(self) -> str:
-        return self.name
+class BaseItem(ABC):
+    def __init__(self, name = None, worth = 0, weight = 0, description = None, **kwargs):
+        if type(self) == BaseItem:
+            raise Exception('Do not instantiate BaseItem directly')
+        self.name = name
+        self.worth = worth
+        self.weight = weight
+        self.description = description
 
-class Rock(Weapon):
-    def __init__(self) -> None:
-        self.name = 'Rock'
-        self.description = "A fist-sized rock, suitable for bludgeoning."
-        self.damage = 5
-        self.value = 1
+    def __repr__(self):
+        return f'{self.__class__.__name__}()'
 
-class Dagger(Weapon):
-    def __init__(self) -> None:
-        self.name = "Dagger"
-        self.description = "A small dagger with some rust. " \
-                            "Somewhat more dangerous than a rock."
-        self.damage = 10
-        self.value = 20
+class CustomWeapon(BaseItem):
+    def __init__(self, damage = 0, **kwargs):
+        super().__init__(**kwargs)
+        self.damage = damage
 
+    def __str__(self):
+        return f'{str(self.name)} damage: {str(self.damage)}'
 
-class RustySword(Weapon):
-    def __init__(self) -> None:
-        self.name = "Rusty sword"
-        self.description = "This sword is showing its age, " \
-                            "but still has some fight in it."
-        self.damage = 20
-        self.value = 100
+class Rock(CustomWeapon):
+    def __init__(self, **kwargs):
+        super().__init__(name = 'Rock',
+                        damage = 5,
+                        worth = 1,
+                        weight = 1,
+                        description = "A fist-sized rock, suitable for bludgeoning.",
+                        **kwargs)
 
-class Crossbow(Weapon):
-    def __init__(self) -> None:
-        self.name = "Crossbow"
-        self.description = "Strong and sturdy Crossbow." \
-                            "Allows a range attack."
-        self.damage = 15
-        self.value = 75
+class Dagger(CustomWeapon):
+    def __init__(self, **kwargs):
+        super().__init__(name = 'Dagger',
+                        damage = 10,
+                        worth = 20,
+                        weight = 1,
+                        description = "A small dagger with some rust. " \
+                            "Somewhat more dangerous than a rock.",
+                        **kwargs)
 
-class Axe(Weapon):
-    def __init__(self) -> None:
-        self.name = "Axe"
-        self.description = "This axe is showing its age, " \
-                            "but still has some fight in it to do damage."
-        self.damage = 25
-        self.value = 60
+class RustySword(CustomWeapon):
+    def __init__(self, **kwargs):
+        super().__init__(name = "Rusty sword",
+                        damage = 20,
+                        worth = 100,
+                        weight = 1,
+                        description = "This sword is showing its age, " \
+                            "but still has some fight in it.",
+                        **kwargs)
 
+class Crossbow(CustomWeapon):
+    def __init__(self, **kwargs):
+        super().__init__(name = "Crossbow",
+                        damage = 15,
+                        worth = 75,
+                        weight = 2,
+                        description = "Strong and sturdy Crossbow." \
+                            "Allows a range attack.",
+                        **kwargs)
 
-class Consumable:
-    def __init__(self) -> None:
-        raise NotImplementedError("Do not create raw Consumable objects.")
+class Axe(CustomWeapon):
+    def __init__(self, **kwargs):
+        super().__init__(name = "Axe",
+                        damage = 25,
+                        worth = 60,
+                        weight = 2,
+                        description = "This axe is showing its age, " \
+                            "but still has some fight in it to do damage.",
+                        **kwargs)
+
+class CustomConsumable(BaseItem):
+    def __init__(self, healing_value = 0, **kwargs):
+        super().__init__(**kwargs)
+        self.healing_value = healing_value
     
     def __str__(self) -> str:
         return "{} (+{} HP)".format(self.name, self.healing_value)
 
-class CrustyBread(Consumable):
-    def __init__(self) -> None:
-        self.name = "Crusty Bread"
-        self.healing_value = 10
-        self.value = 12
+class CrustyBread(CustomConsumable):
+    def __init__(self, **kwargs):
+        super().__init__(name = "Crusty Bread",
+                        healing_value = 10,
+                        worth = 12,
+                        weight = 1,
+                        description = "Crusty Bread that is not fresh.",
+                        **kwargs)
 
-class HealingPotion(Consumable):
-    def __init__(self) -> None:
-        self.name = "Healing Potion"
-        self.healing_value = 50
-        self.value = 60
+class HealingPotion(CustomConsumable):
+    def __init__(self, **kwargs):
+        super().__init__(name = "Healing Potion",
+                        healing_value = 50,
+                        worth = 60,
+                        weight = 1,
+                        description = "Red shiny liquid in a glass bottle.",
+                        **kwargs)
+
+
+if __name__ == "__main__":
+    # BaseItem()
+    
+    w = Rock()
+    d = Dagger()
+    print(w, d)
+    print([w, d])
+    print(w.worth)
