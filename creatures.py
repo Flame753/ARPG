c8d66ac566348a7e936d8dd641c5ec9b0435a12d
Flame753 <Sgurman753@gmail.com>
2021-12-25 20:47:31 -0500

    Updated how slots is created

--- creatures.py
+++ creatures.py
@@ -2,31 +2,51 @@ from abc import ABC
 
 
 # Equipment Slots that are Exist
-EQUIP_SLOT = ['EQUIP_SLOT_HEAD',
-            'EQUIP_SLOT_NECK',
-            'EQUIP_SLOT_TORSO',
-            'EQUIP_SLOT_WAIST',
-            'EQUIP_SLOT_LEGS',
-            'EQUIP_SLOT_FEETS',
-            'EQUIP_SLOT_MAIN_HAND',
-            'EQUIP_SLOT_OFF_HAND',
-            'EQUIP_SLOT_RING',
-            'EQUIP_SLOT_TAIL',
-            'EQUIP_SLOT_WINGS',]
+EQUIP_SLOTS = {'heads': ['EQUIP_SLOT_HEAD',
+                    'EQUIP_SLOT_NECK',],
+            'eyes': [],
+            'body': ['EQUIP_SLOT_TORSO',
+                    'EQUIP_SLOT_WAIST',],
+            'legs': ['EQUIP_SLOT_LEGS',
+                    'EQUIP_SLOT_FEETS',],
+            'hands': ['EQUIP_SLOT_MAIN_HAND',
+                    'EQUIP_SLOT_OFF_HAND',],
+            'fingers': ['EQUIP_SLOT_RING'],
+            'tails': ['EQUIP_SLOT_TAIL'],
+            'wings': ['EQUIP_SLOT_WINGS']}
+
+def GET_EQUIP_SLOTS():
+    slots = list()
+    for slot in EQUIP_SLOTS.values():
+        slots.extend(slot)
+    return slots
 
 
-class EquipSlot(ABC):
-    def __init__(self):
-        if type(self) == EquipSlot:
-            raise Exception('Do not instantiate EquipSlot directly')
-        self.slots = list()
+class Creature(ABC):
+    def __init__(self, heads=0, eyes=0, body=0, legs=0, hands=0, fingers=0, tails=0, wings=0, **kwargs):
+        if type(self) == Creature:
+            raise Exception('Do not instantiate Creature directly')
+        self.heads = heads
+        self.eyes = eyes//2
+        self.body = body
+        self.legs = legs//2
+        self.hands = hands//2
+        self.fingers = fingers//max(1, hands)
+        self.tails = tails
+        self.wings = wings
+
+        self.slot_list = list()
+
+    def define_slots(self):
+        for bodypart, slot in EQUIP_SLOTS.items():
+            self.slot_list.extend(slot*self.__dict__.get(bodypart))
 
     def ensure_equipment_slots(self):
         if not hasattr(self, 'equipment_slots'):
             self.equipment_slots = {}
 
     def slots_exist(self, slot):
-        return True if slot in self.slots else False
+        return True if slot in self.slot_list else False
 
     def slot_available(self, slot):
         self.ensure_equipment_slots()
@@ -35,7 +55,6 @@ class EquipSlot(ABC):
         # Checks if an item is occupying the slot
         return False if self.equipment_slots.get(slot, None) else True
 
-
     def add_equipment(self, item, slot):
         self.ensure_equipment_slots()
 
@@ -43,40 +62,6 @@ class EquipSlot(ABC):
             self.equipment_slots[slot] = item
             return True
         return False
-
-    # def add_equipments(self, item, slots):
-    #     if type(item) == item and type(slots) == list:
-    #         for num, loc in enumerate(slots):
-    #             print(loc)
-    #             if loc in self.slots:
-    #                 pass
-
-
-    # def change_equipment(self, **kwargs):
-    #     self.ensure_equipment_slots()
-
-    #     # Changes current equipment out with new one
-    #     for key, value in kwargs.items():
-    #         if key in self.equimplment_slots:
-    #             pass
-    #         if value:
-    #             # Returns Equipment back into bag
-    #             self.bag.addItem(self.__dict__[key], 1)
-    #     self.__dict__.update(kwargs)
-    #     return True
-
-class Creature(EquipSlot):
-    def __init__(self, heads=1, eyes=2, body=1, legs=2, hands=2, fingers=10, tails=0, wings=0, **kwargs):
-        if type(self) == Creature:
-            raise Exception('Do not instantiate Creature directly')
-        self.heads = heads
-        self.eyes = eyes
-        self.body = body
-        self.legs = legs
-        self.hands = hands
-        self.fingers = fingers
-        self.tails = tails
-        self.wings = wings
 
     def is_alive(self):
         return self.hp > 0
@@ -88,20 +73,11 @@ class Creature(EquipSlot):
         return True
 
 class Humonoid(Creature):
-    def __init__(self, **kwargs):
-        super().__init__(self, **kwargs)
+    def __init__(self, heads=1, eyes=2, legs=2, hands=2, fingers=10, **kwargs):
+        super().__init__(heads=heads, eyes=eyes, legs=legs, hands=hands, fingers=fingers, **kwargs)
         if type(self) == Humonoid:
             raise Exception('Do not instantiate Humonoid directly')
 
-        self.slots = ['EQUIP_SLOT_HEAD',
-                    'EQUIP_SLOT_NECK',
-                    'EQUIP_SLOT_TORSO',
-                    'EQUIP_SLOT_WAIST',
-                    'EQUIP_SLOT_LEGS',
-                    'EQUIP_SLOT_FEETS',
-                    'EQUIP_SLOT_MAIN_HAND',
-                    'EQUIP_SLOT_OFF_HAND',
-                    'EQUIP_SLOT_RING']
 
 class Beast(Creature):
     def __init__(self, **kwargs):
@@ -109,21 +85,6 @@ class Beast(Creature):
         if type(self) == Beast:
             raise Exception('Do not instantiate Beast directly')
         
-        self.slots = ['EQUIP_SLOT_HEAD',
-            'EQUIP_SLOT_NECK',
-            'EQUIP_SLOT_TORSO',
-            'EQUIP_SLOT_WAIST',
-            'EQUIP_SLOT_LEGS',
-            'EQUIP_SLOT_FEETS',
-            'EQUIP_SLOT_LEGS',
-            'EQUIP_SLOT_FEETS', 
-            'EQUIP_SLOT_TAIL']
 
 if __name__ == '__main__':
-    import items
-    c = Creature()
-    print(c.slots_exist('EQUIP_SLOT_TORSO'))
-    c.add_equipment(items.HealingPotion(), 'EQUIP_SLOT_TORSO')
-    print(c.equipment_slots)
-    print(c.slot_available('EQUIP_SLOT_TORSO'))
-    # c.addEquipment(items.Dagger(), ['EQUIP_SLOT_TAIL'])
+    pass
