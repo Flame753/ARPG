547de196a4b2ee336fcf21ae598571a3b8152d45
Flame753 <Sgurman753@gmail.com>
2022-03-31 18:28:40 -0400

    renamed inventory to be hidden

--- arpg/entities/utils.py
+++ arpg/entities/utils.py
@@ -4,55 +4,55 @@ from abc import ABC
 
 class Container(ABC):
     def _ensure_inventory(self) -> None:
-        if not hasattr(self, 'inventory'):
-            self.inventory = dict()
+        if not hasattr(self, '_inventory'):
+            self._inventory = dict()
 
     def get_inventory(self):
         self._ensure_inventory()
-        return self.inventory
+        return self._inventory
 
     def add_item(self, item, amount: int = 0) -> None:
         self._ensure_inventory()
 
         if amount < 0: raise ValueError
 
-        if item in self.inventory:
-            self.inventory[item]['amount'] += amount
+        if item in self._inventory:
+            self._inventory[item]['amount'] += amount
         else:
-            self.inventory[item] = {'amount': amount}
+            self._inventory[item] = {'amount': amount}
 
     def remove_item(self, item, amount: int = 0) -> bool:
         self._ensure_inventory()
 
         if amount < 0: raise ValueError
 
-        if item in self.inventory:
-            if self.inventory[item]['amount'] >= amount:
-                self.inventory[item]['amount'] -= amount
-                if self.inventory[item]['amount'] == 0:
-                    del(self.inventory[item])
+        if item in self._inventory:
+            if self._inventory[item]['amount'] >= amount:
+                self._inventory[item]['amount'] -= amount
+                if self._inventory[item]['amount'] == 0:
+                    del(self._inventory[item])
                 return True
         return False
 
     def calculate_item_weight(self, item) -> int:
         self._ensure_inventory()
 
-        if item in self.inventory:
-            return item.weight * self.inventory[item]['amount']
+        if item in self._inventory:
+            return item.weight * self._inventory[item]['amount']
         return 0
 
     def calculate_item_worth(self, item)  -> int:
         self._ensure_inventory()
 
-        if item in self.inventory:
-            return item.worth * self.inventory[item]['amount']
+        if item in self._inventory:
+            return item.worth * self._inventory[item]['amount']
         return 0
 
     def calculate_total_weight(self) -> int:
         self._ensure_inventory()
 
         weight = 0
-        for item, data in self.inventory.items():
+        for item, data in self._inventory.items():
             weight += item.weight * data['amount']
         return weight
 
@@ -60,6 +60,6 @@ class Container(ABC):
         self._ensure_inventory()
 
         worth = 0
-        for item, data in self.inventory.items():
+        for item, data in self._inventory.items():
             worth += item.worth * data['amount']
         return worth
