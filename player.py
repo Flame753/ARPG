371110154b2dae15aa6ec252c52b709f08656f8f
Flame753 <Sgurman753@gmail.com>
2022-05-30 20:16:16 -0400

    added max hp

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(Container):
     def __init__(self, name:str, hp:int = 10) -> None:
         self.name = name
         self.hp = hp
+        self.max_hp = hp
 
     def is_alive(self) -> bool:
         return self.hp > 0
