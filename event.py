a54b1245539362aa681b4c05a6f3ab9601812226
Flame753 <Sgurman753@gmail.com>
2022-06-03 15:41:17 -0400

    Save change to storing data in the event

--- event.py
+++ event.py
@@ -1,6 +1,6 @@
 # Standard library imports 
 from dataclasses import dataclass, field, fields, astuple
-from typing import Optional
+from typing import Optional, Protocol
 import random
 import enum
 
@@ -36,7 +36,7 @@ class RotatingDecision:
 
 @dataclass(frozen=True)
 class Effect():
-    result_text: str
+    text: str = field(repr=False)
 
     def add_requirement(self, requirement) -> None:
         if not hasattr(requirement, "requirement"):
@@ -75,6 +75,38 @@ class LossItem(ReceiveReward):
         player.remove_item(self.item, self.amount)
 
 
+# class Effect(Protocol):
+#     # text: str = field(repr=False)
+
+#     # def add_requirement(self, requirement) -> None:
+#     #     if not hasattr(requirement, "requirement"):
+#     #         self.requirement = [requirement]
+#     #     else:
+#     #         self.requirement.append(requirement)
+        
+#     def modify_player(self, player: Player) -> None:
+#         ...
+
+# class TakesDamage(Effect):
+#     def modify_player(self, player: Player, damage: int) -> None:
+#         player.hp -= damage
+
+# class RestoresHealth(Effect):
+#     def modify_player(self, player: Player, damage: int) -> None:
+#         player.hp += damage
+#         if player.hp > player.max_hp:
+#             player.hp = player.max_hp
+
+# class ReceiveReward(Effect):
+#     def modify_player(self, player: Player, item: str, amount: int) -> None:
+#         player.add_item(item, amount)
+
+# class LossItem(Effect):
+#     def modify_player(self, player: Player, item: str, amount: int) -> None:
+#         player.remove_item(item, amount)
+
+
+
 def _vaild_arguments(influence: Effect, outcome: Effect, next_influence: Optional[Effect]) -> None:
     if not isinstance(influence, Effect):
         raise TypeError(f"{influence} unvalid argument for the influence perimeter!")
@@ -95,72 +127,65 @@ class Event:
         if not hasattr(self, "_data"):
             self._data = dict()
 
-    def _setup_event(self, influence: Effect):
+    def _setup_influence(self, influence: Effect):
         self._data[influence] = {"auto assign": RotatingDecision()}
  
-    def set_starting_link(self,  influence: Effect) -> None:
+    def set_initial_link(self,  influence: Effect) -> None:
         self._ensure_data()
-        if self._data:
-            raise Exception("This method was already once used!")
-        self._setup_event(influence)
-        self.next_influence = influence
+        self._setup_influence(influence)
 
-    def set_decision(self, influence: Effect, outcome: Effect, next_influence: Optional[Effect]=None):
+
+    def set_decision(self, influence: Effect, decision_text: str ,outcome: Effect, next_influence: Optional[Effect]=None):
         _vaild_arguments(influence, outcome, next_influence)
+        self.set_initial_link(influence)
 
-        if not hasattr(self, "_data"):
-            raise Exception("<set_starting_link must be called before this method is called!>")
-
-        event_decision = self._data.get(influence)
-        if event_decision == None:
-            raise Exception(f"The entered influence <{influence}> doesn't exist! Try linking this event with an existing event, first!")
-
+        vaild_decision = self._data.get(influence)
         try:
-            option = event_decision.get("auto assign").next_decision()
+            an_option = vaild_decision.get("auto assign").next_decision()
         except StopIteration:
             raise Exception(f"Limit was reached! Max limit is ({len(Decision)}) differetn outcomes!")
         
-        event_decision[option] = {"outcome": outcome, "linked_influence": next_influence}
+        vaild_decision[an_option] = {"text": decision_text, "outcome": outcome, "linked": next_influence}
 
         if next_influence:
-            self._setup_event(next_influence)
+            self._setup_influence(next_influence)
 
 
-    def _ensure_next_event(self):
-        if not hasattr(self, "next_influence"):
-            raise Exception("No starting event was set!")
+    # def _ensure_next_event(self):
+    #     if not hasattr(self, "next_influence"):
+    #         raise Exception("No starting event was set!")
 
-    def determine_players_outcome(self, current_influence: Effect) -> Optional[Effect]:
-        self._ensure_data()
-        # create error for not finshing adding dicisions to all events
-        for event_data in self._data.values():
-            if not any([event_data.get(d) for d in Decision]):
-                raise Exception("An event was linked to another event. However, wan't properly filled out!")
+    # def determine_players_outcome(self, current_influence: Effect) -> Optional[Effect]:
+    #     self._ensure_data()
+    #     # create error for not finshing adding dicisions to all events
+    #     for event_data in self._data.values():
+    #         if not any([event_data.get(d) for d in Decision]):
+    #             raise Exception("An event was linked to another event. However, wan't properly filled out!")
 
-        self.ui.display_text(current_influence.result_text)
-        options = dict()
-        for dicision in Decision:
-            outcome = self._data.get(current_influence).get(dicision)
-            if outcome:
-                print("valid input ", dicision.name) # Temp Line
-                utils.action_adder(options, dicision.name, outcome)
+    #     self.ui.display_text(current_influence.text)
+    #     options = dict()
+    #     for dicision in Decision:
+    #         outcome = self._data.get(current_influence).get(dicision)
+    #         if outcome:
+    #             print("valid input ", dicision.name) # Temp Line
+    #             utils.action_adder(options, dicision.name, outcome)
 
-        player_dicision = self.ui.interact_with_user(options)
-        self.ui.display_text(player_dicision.get("outcome").result_text)
-        player_dicision.get("outcome").modify_player(self.player)
-        return player_dicision.get("linked_influence")
+    #     player_dicision = self.ui.interact_with_user(options)
+    #     self.ui.display_text(player_dicision.get("outcome").text)
+    #     player_dicision.get("outcome").modify_player(self.player)
+    #     return player_dicision.get("linked")
         
 
-    def __iter__(self):
-        return self
+    # def __iter__(self):
+    #     return self
 
-    def __next__(self):
-        self._ensure_next_event()
-        current_influence = self.next_influence
-        self.next_influence = self.determine_players_outcome(current_influence)
-        if self.next_influence == None:
-            raise StopIteration
-        return current_influence
+    # def __next__(self):
+    #     self._ensure_next_event()
+    #     current_influence = self.next_influence
+    #     self.next_influence = self.determine_players_outcome(current_influence)
+    #     if self.next_influence == None:
+    #         raise StopIteration
+    #     return current_influence
 
 
 
@@ -198,18 +223,18 @@ def main():
     ui = cli.CLI()
 
     Influence1 = Effect("You were walking down a path and see a carriage with a broken wheel on the side of the road. ")
-    outcome1 = ReceiveReward(result_text="After spending a few hours fixing the carriage. " \
+    outcome1 = ReceiveReward(text="After spending a few hours fixing the carriage. " \
                                                             "A old man steps out of the carriage and thanks you for fixing his carriage. " \
                                                             "He gives you 2 small gold coins. ", item=Currency.Gold, amount=2)
     e2 = Effect("event_2")
 
     s = Event(name="test", ui=ui, player=player)
-    s.set_starting_link(influence=Influence1)
-    s.set_decision(Influence1, outcome1, e2)
-    s.set_decision(e2, PlayerTakesDamage(result_text="player got damaged", damage=3))
-    for _ in s:
-        pass
-    print(player.hp)
+    s.set_initial_link(influence=Influence1)
+    s.set_decision(Influence1, "test is here",outcome1, e2)
+    s.set_decision(e2,"text is here", PlayerTakesDamage(text="player got damaged", damage=3))
+    # for _ in s:
+    #     pass
+    # print(player.hp)
     pprint(s._data)
 
     # print({x:2 for x in range(10)})
